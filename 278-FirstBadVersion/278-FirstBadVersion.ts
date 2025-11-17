8370d826d8f9716b4bf9383b7837b1f5592e901c
Nathan Xu <space.man0318@gmail.com>
2025-11-17 16:37:51 +0800

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/278-FirstBadVersion.ts
@@ -0,0 +1,25 @@
+// Last updated: 11/17/2025, 4:37:50 PM
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function (isBadVersion: any) {
+  return function (n: number): number {
+    let left = 1,
+      right = n
+
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      if (isBadVersion(mid)) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return left;
+  };
+};
