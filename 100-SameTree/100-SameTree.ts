f6d139919b75b2367718afd2517cef0d4a7cdf89
Nathan Xu <space.man0318@gmail.com>
2025-11-17 16:38:26 +0800

    Create 100-SameTree

---
+++ 100-SameTree/100-SameTree.ts
@@ -0,0 +1,24 @@
+// Last updated: 11/17/2025, 4:38:25 PM
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (p && q && p.val == q.val) {
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  } else if (!p && !q) {
+    return true;
+  } else {
+    return false;
+  }
+}
